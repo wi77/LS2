035d1ccad5050ec5910904d1f55fee7c50e3bace
Marcel Kyas <marcel.kyas@googlemail.com>
2013-06-27 08:43:16 +0200

    Some smaller clean-ups to help in debugging.

--- src/algorithm/rlsm_algorithm.c
+++ src/algorithm/rlsm_algorithm.c
@@ -51,71 +51,77 @@
 #include "algorithm/nllsq_algorithm.c"
 
 
-static inline int 
+static inline int
 __attribute__((__always_inline__,__gnu_inline__,__artificial__,__nonnull__))
-toIndex(int row, int col, int N) {
-        int idx = -1;
-        if (row < col) {
-            idx = row * (N-1) - (row-1) * ((row-1) + 1)/2 + col - row - 1;
-        } else if (col < row) {
-            idx = col * (N-1) - (col-1) * ((col-1) + 1)/2 + row - col - 1;
-        }
-        return idx;
+toIndex(int row, int col, size_t N)
+{
+    assert(row != col);
+    int idx = -1;
+    if (row < col) {
+        idx = row * ((int)N-1) - (row-1) * ((row-1) + 1)/2 + col - row - 1;
+    } else if (col < row) {
+        idx = col * ((int)N-1) - (col-1) * ((col-1) + 1)/2 + row - col - 1;
     }
-    
-static inline int 
-__attribute__((__always_inline__,__gnu_inline__,__artificial__,__nonnull__))
-robust_filter(int const count ,float *restrict resx,float *restrict resy) {
-        int N = count * (count - 1) / 2;
-        float v[N];
-        for (int i = 0; i < count-1; i++) {
-            for (int j = i+1; j < count; j++) {
-                int idx = toIndex(i, j, count);
-                v[idx] = distance_s(resx[i],resy[i],resx[j],resy[j]);
-            }
-        }
+    return idx;
+}
 
-        float MEDV = 2.0f * select_s((size_t) N, v, (size_t) N / 2u + 1u);
-        float filtered_x[count];
-        float filtered_y[count];
-        int filtered_count = 0;
-        for (int i = 0; i < count; i++) {
-            int dropCounter = 0;
-            for (int j = 0; j < count; j++) {
-                if (i == j) {
-                    continue;
-                }
-                float dist = v[toIndex(i, j, count)];
-                if (dist >= MEDV) {
-                    dropCounter++;
-                }
+static inline size_t
+__attribute__((__always_inline__,__gnu_inline__,__artificial__,__nonnull__))
+robust_filter(size_t const count ,float *restrict resx,float *restrict resy)
+{
+    assert (count > 1);
+    const size_t N = count * (count - 1) / 2;
+    assert (N < INT_MAX);
+    float v[N];
+    for (int i = 0; i < (int)count - 1; i++) {
+        for (int j = i+1; j < (int)count; j++) {
+            const int idx = toIndex(i, j, count);
+            v[idx] = distance_s(resx[i], resy[i], resx[j], resy[j]);
+        }
+    }
+
+    const float MEDV = 2.0f * select_s(N, v, N / 2u + 1u);
+    float filtered_x[count];
+    float filtered_y[count];
+    size_t filtered_count = 0;
+    for (int i = 0; i < (int) count; i++) {
+        size_t dropCounter = 0;
+        for (int j = 0; j < (int) count; j++) {
+            if (i == j) {
+                continue;
             }
-            if (dropCounter <= count/2) {
-                filtered_x[filtered_count] = resx[i];
-                filtered_y[filtered_count] = resy[i];
-                filtered_count++;
+            float const dist = v[toIndex(i, j, count)];
+            if (dist >= MEDV) {
+                dropCounter++;
             }
         }
-        memcpy(resx,filtered_x,sizeof(float)*(unsigned int)filtered_count);
-      	memcpy(resy,filtered_y,sizeof(float)*(unsigned int)filtered_count);
-        return filtered_count;
-   }    
+        if (dropCounter <= count/2) {
+            filtered_x[filtered_count] = resx[i];
+            filtered_y[filtered_count] = resy[i];
+            filtered_count++;
+        }
+    }
+    assert(filtered_count > 0);
+    memcpy(resx,filtered_x,sizeof(float) * filtered_count);
+    memcpy(resy,filtered_y,sizeof(float) * filtered_count);
+    return filtered_count;
+}    
 
 
 static inline void
 __attribute__((__always_inline__,__gnu_inline__,__artificial__,__nonnull__))
 rlsm_run(const VECTOR* vx, const VECTOR* vy, const VECTOR *restrict r,
-              size_t no_anchors, int width, int height,
-              VECTOR *restrict resx, VECTOR *restrict resy)
+         size_t no_anchors,
+         int width __attribute__((__unused__)),
+         int height __attribute__((__unused__)),
+         VECTOR *restrict resx, VECTOR *restrict resy)
 {
-    if (width==height){};
-
-    int s = 3;
-    int M = (int)no_anchors;
+    const int s = 3;
+    const int M = (int)no_anchors;
     
     for (int ii = 0; ii < VECTOR_OPS; ii++) {
         // calculate all k-permutations for k = s to 
-        int int_count = 0;
+        size_t int_count = 0;
         int ccount = 0;
         for (int k = s; k <= M; k++) {
             ccount += binom(M, k);
@@ -176,21 +182,21 @@ rlsm_run(const VECTOR* vx, const VECTOR* vy, const VECTOR *restrict r,
             }
         }
 
-        float x_x = 0.0f;
-        float x_y = 0.0f;
+        float x_x = NAN;
+        float x_y = NAN;
 
         // apply robust median filter to this array of intermediate
         // position estimates
-        if (int_count > 1) {
+        if (int_count > 1u) {
             int_count = robust_filter(int_count, intermediatePositions_x, intermediatePositions_y);
         }
         
         float weights[int_count];
-        for (int jj=0;jj<int_count;jj++) {
-	         weights[jj]=1.0f;
-	    }
+        for (size_t jj = 0; jj < int_count; jj++) {
+	    weights[jj] = 1.0f;
+	}
         // return geometric median as result
-        point_geometric_median(int_count, intermediatePositions_x,
+        point_geometric_median((int) int_count, intermediatePositions_x,
                        intermediatePositions_y,
                        weights,
                        &x_x, &x_y);
