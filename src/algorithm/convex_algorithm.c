3c9168ab261a4dbce517a3c31c91217631872348
Paul Podlech <p.podlech@fu-berlin.de>
2013-09-16 02:15:06 +0200

    test algorithm added as error output help

--- src/algorithm/convex_algorithm.c
+++ src/algorithm/convex_algorithm.c
@@ -31,15 +31,17 @@ along with LS².  If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef CONVEX_ALGORITHM_C_INCLUDED
 #define CONVEX_ALGORITHM_C_INCLUDED 1
-#define CONVEX_EPS 0.01f
+
+#define CONVEX_EPS 1.0f
 
 #include "util/util_circle.c"
 #include "util/util_vector.c"
 
 /*
-* This function finds a point, if existing, in the intersection of
+* This function finds a point in the intersection of
 * the no_anchors given open disks with center points
 * (vx[i][0],vy[i][0]) and radii r[i][0].
+* If no such point exists it the result is (NAN,NAN).
 * Runtime is in O(no_anchors^3). Additional used space is in O(1).
 */
 static inline void __attribute__((__always_inline__,__gnu_inline__,__nonnull__,__artificial__))
@@ -50,19 +52,19 @@ int height __attribute__((__unused__)),
 VECTOR *restrict resx,
 VECTOR *restrict resy)
 {
-	// first check if two disks are disjoint,
+	// First check if two disks are disjoint,
 	// or if one centerpoint is inside of all disks
 	double dist = 0.0f;
-	char onecircle = 1;
+	char centerinside = 1;
 	
 	for(size_t i = 0; i < no_anchors; i++){
-		onecircle = 1;
+		centerinside = 1;
 		for(size_t j = 0; j < no_anchors; j++){
 			if(i == j)
-				continue;
+			continue;
 			dist = distance_squared_sf(vx[i][0],vy[i][0],vx[j][0],vy[j][0]);
 			// If and only if  
-			if(dist >= r[i][0]*r[i][0]+ 2*r[i][0]*r[j][0]  + r[j][0])*r[j][0]){
+			if(dist >= r[i][0]*r[i][0]+ 2*r[i][0]*r[j][0]  + r[j][0]*r[j][0]){
 				(*resx)[0] = NAN;
 				(*resy)[0] = NAN;
 				return;
@@ -70,13 +72,13 @@ VECTOR *restrict resy)
 			// If d(A_i,A_j) >= r_j, then
 			// A_i is not in B_j.
 			if(dist >= r[j][0]*r[j][0]){
-				onecircle = 0;
+				centerinside = 0;
 			}
 
 		}
 		// return the centerpoint if it
 		// already is in I
-		if(onecircle){
+		if(centerinside){
 			(*resx)[0] = vx[i][0];
 			(*resy)[0] = vy[i][0];
 			return;
@@ -101,85 +103,97 @@ VECTOR *restrict resy)
 	resy1[0] = NAN;
 	resx1[1] = NAN;
 	resy1[1] = NAN;
-	char nonefound = 1;
+	size_t found = 0;
+	char insideflag;
 	
 	
 	
 
 	// Loop over each pair of circles.
-	for (size_t i = 0; i < no_anchors; i++){
-		for(size_t j = 0; j < no_anchors; j++){
-			if(i == j)
-				continue;
+	for (size_t i = 0; i < no_anchors - 1; i++){
+		for(size_t j = i+1; j < no_anchors; j++){
 			// find the intersection point/points
-			no_res = circle_get_intersectionf(vx[i][0],vy[i][0],r[i][0],vx[j][0],vy[j][0],r[j][0],resx1,resy1);
+			no_res = circle_get_intersectionf(vx[i][0],vy[i][0],vx[j][0],vy[j][0],r[i][0],r[j][0],resx1,resy1);
+			if(no_res == 0){
+				// happens if one circle is inside of the other
+				continue;
+			}
 			if(no_res == 1){
+				// should never happen
+				fprintf(stderr,"1 intersection point. Unlikely.\r\n");
 				// if only one intersection point is found
 				// then go to the code for the first intersection point
 				goto int1;
 			}
 			
+			insideflag = 1;
 			// check if the second intersection point
 			// is inside of all other closed disks
 			for(size_t k = 0; k < no_anchors; k++){
 				// skip the circles from the outer loop
 				if(k == i || k == j)
-					continue;
+				continue;
 				dist = distance_squared_sf(vx[k][0],vy[k][0],resx1[1],resy1[1]);
-				if(dist > r[k][0]*r[k][0] + CONVEX_EPS){
-					goto int1;
+				if(dist > r[k][0]*r[k][0]){
+					insideflag = 0;
+					break;
 				}
 			}
 			
-			// Arriving here, we have found a point
-			if(nonefound){
-				// if its the first one, write it to index 0
-				resultx[0] = resx1[1];
-				resulty[0] = resy1[1];
-				nonefound = 0;
-			}
-			else{
-				// if its the second one, write it to index 1
-				// and go to the end
-				resultx[1] = resx1[1];
-				resulty[1] = resy1[1];
-				goto end;
-			}
-
+			if(insideflag){
+				// Arriving here, we have found a point.
+				// Write the result to the appropriate index.
+					resultx[found] = resx1[1];
+					resulty[found] = resy1[1];
+					if(found == 1){
+						if(distance_squared_sf(resultx[0],resulty[0],resultx[1],resulty[1]) > CONVEX_EPS)
+							found++;
+					}
+					else
+						found++;
+					if(found == 2)
+						goto end;
+				}
+			
 			// do the same for the first intersection point
 int1: 
+// this label is just here for the case of one intersection point
+// and one circle is inside the other
+
+			insideflag = 1;
 			for(size_t k = 0; k < no_anchors; k++){
 				// skip the circles from the outer loop
 				if(k == i || k == j)
-					continue;
+				continue;
 				dist = distance_squared_sf(vx[k][0],vy[k][0],resx1[0],resy1[0]);
-				if(dist > r[k][0]*r[k][0] + CONVEX_EPS){
-					goto cont;
+				if(dist > r[k][0]*r[k][0]){
+					insideflag = 0;
+					break;
 				}
 			}
-			// Arriving here, we have found a point
-			if(nonefound){
-				// if its the first one, write it to index 0
-				resultx[0] = resx1[0];
-				resulty[0] = resy1[0];
-			}
-			else{
-				// if its the second one, write it to index 1
-				// and go to the end
-				resultx[1] = resx1[0];
-				resulty[1] = resy1[0];
-				goto end;
+			if(insideflag){
+				// Arriving here, we have found a point.
+				// Write the result to the appropriate index.
+					resultx[found] = resx1[0];
+					resulty[found] = resy1[0];
+					if(found == 1){
+						if(distance_squared_sf(resultx[0],resulty[0],resultx[1],resulty[1]) > CONVEX_EPS)
+							found++;
+					}
+					else
+						found++;
+					if(found == 2)
+						goto end;
+				}
 			}
 		}
-cont: ;
-	}
+	fprintf(stderr, "error\r\n");	
 	// Arriving here means no two points
 	// on the boundary of the intersection
 	// of the open disks have been found.
 	// That means the intersection is empty.
 	(*resx)[0] = NAN;
 	(*resy)[0] = NAN;	
-	fprintf(convex_file,"((x-%f)^2 + (y - %f)^2 < %f) && ((x-%f)^2 + (y - %f)^2 < %f) && ((x-%f)^2 + (y - %f)^2 < %f) && ((x-%f)^2 + (y - %f)^2 < %f)", vx[0][0], vy[0][0], r[0][0], vx[1][0], vy[1][0], r[1][0], vx[2][0], vy[2][0], r[2][0], vx[3][0], vy[3][0], r[3][0]);
 	return;
 
 	// Here we found two according points.
@@ -192,30 +206,38 @@ end:
 }
 
 
-// pixel iterator algorithm
+// This algorithm finds a discrete point (a grid point) inside
+// of the given disks, if it exists.
+// It's very slow, because it every time iterates over all possible
+// grid points and checks if they are results.
+// Can be used if other approaches fail.
 static inline void __attribute__((__always_inline__,__gnu_inline__,__nonnull__,__artificial__))
-pixel_run(const VECTOR* vx, const VECTOR* vy, const VECTOR *restrict r,
+convex_pixel(const VECTOR* vx, const VECTOR* vy, const VECTOR *restrict r,
 size_t no_anchors,
-int width __attribute__((__unused__)),
-int height __attribute__((__unused__)),
+int width,
+int height,
 VECTOR *restrict resx,
 VECTOR *restrict resy)
 {
-	for(int i = 0; i < 1000; i ++){
-		for(int j = 0; j < 1000; j++){
-			char inside = 1;
+	char inside = 1;
+	float dist = 0.0f;
+	for(float i = 0.0f; i < width; i++){
+		for(float j = 0.0f; j < height; j++){
+			inside = 1;
 			for(size_t k = 0; k < no_anchors; k++){
-				if(distance_s(i,j,vx[k][0],vy[k][0]) > r[k][0]){
+				dist = (i - vx[k][0])*(i - vx[k][0]) + (j - vy[k][0])*(j + vy[k][0]);
+				if(dist >= r[k][0]*r[k][0]){
 					inside = 0;
+					break;
 				}
 			}
-			if(inside){
+			if(inside == 1){
 				(*resx)[0] = (float)i;
 				(*resy)[0] = (float)j;
+				return;
 			}
 		}
 	}
-	fprintf(convex_file,"((x-%f)^2 + (y - %f)^2 < %f) && ((x-%f)^2 + (y - %f)^2 < %f) && ((x-%f)^2 + (y - %f)^2 < %f) && ((x-%f)^2 + (y - %f)^2 < %f)", vx[0][0], vy[0][0], r[0][0]*r[0][0], vx[1][0], vy[1][0], r[1][0]*r[1][0], vx[2][0], vy[2][0], r[2][0]*r[2][0], vx[3][0], vy[3][0], f[3][0]*r[3][0]);
 	return;
 }
 
